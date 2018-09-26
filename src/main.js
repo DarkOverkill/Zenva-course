b5e80b419208ce96ec3aecbd3762433e0ac96a4e
Max <maxym.fedas@gmail.com>
2018-09-26 17:44:03 +0300

    added Boot, Load, Title scene

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
-let gameScene = new Phaser.Scene('Title');
+import TitleScene from './scenes/TitleScene';
+import BootScene from './scenes/BootScene';
+import LoadingScene from './scenes/LoadingScene';
+
+let titleScene = new TitleScene();
+let bootScene = new BootScene();
+let loadingScene = new LoadingScene();
 
 let config = {
   type: Phaser.AUTO,
   width: 640,
-  heigth: 360,
-  scene: gameScene
+  height: 360,
 };
 
 let game = new Phaser.Game(config);
+game.scene.add('TitleScene', titleScene);
+game.scene.add('BootScene', bootScene);
+game.scene.add('LoadingScene', LoadingScene);
+game.scene.start('BootScene', { scene: 'title' });
 
